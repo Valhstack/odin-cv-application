29dffdbec59560ed8e694e13f6d3a398f9227af6
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2026-05-12 20:36:14 +0200

    feat: added generalInfo section
    working on creating a basic structure of the app: navbar, sidebar, and preview sections
    currently, have a form to add general information (full name, email, and phone number)
    which also updates in the preview section automatically on each key stroke
    
    next step:
     - finish adding education information

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,108 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Navbar() {
+  return (
+    <nav id='navbar'>CV Generator</nav>
+  )
+}
+
+function GeneralInfo({ generalInfo, onFullNameChange, onEmailChange, onPhoneNumberChange }) {
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
+    <div id='general-info-wrapper'>
+      <button id='general-info-btn' onClick={() => setIsExpanded(!isExpanded)}>General Information</button>
+      <div id='general-info-form' className={!isExpanded ? 'inactive' : 'general-info-form'}>
+        <div className="label-input-wrapper">
+          <label for='full-name'>Full Name: </label>
+          <input type='text' name='full-name' id='full-name' value={generalInfo.fullName} onChange={onFullNameChange} placeholder='John Dou'></input>
+        </div>
+        <div className="label-input-wrapper">
+          <label for='email'>Email: </label>
+          <input type='email' name='email' id='email' value={generalInfo.email} onChange={onEmailChange} placeholder='john.dou@emaildomain.com'></input>
+        </div>
+        <div className="label-input-wrapper">
+          <label for='phone-number'>Phone number: </label>
+          <input type='tel' name='phone-number' id='phone-number' value={generalInfo.phoneNumber} onChange={onPhoneNumberChange} placeholder='+1 234 567 8910'></input>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+/*function EducationInfo() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div id='education-info-wrapper'>
+      <button id='education-info-btn' onClick={() => setIsExpanded(!isExpanded)}>Education Information</button>
+      <div id='education-info-form' className={!isExpanded ? 'inactive' : ''}>
+
+      </div>
+    </div>
+  )
+}*/
+
+function Sidebar({ generalInfo, onFullNameChange, onEmailChange, onPhoneNumberChange }) {
+  return (
+    <div id='sidebar'>
+      <GeneralInfo generalInfo={generalInfo} onFullNameChange={onFullNameChange} onEmailChange={onEmailChange} onPhoneNumberChange={onPhoneNumberChange} />
+    </div>
+  )
+}
+
+function Preview({ generalInfo }) {
+  return (
+    <div id='preview'>
+      <p>{generalInfo.fullName}</p>
+      <p>{generalInfo.email}</p>
+      <p>{generalInfo.phoneNumber}</p>
+    </div>
+  )
+}
+
+function Main() {
+  const [generalInfo, setGeneralInfo] = useState({
+    fullName: '',
+    email: '',
+    phoneNumber: ''
+  })
+
+  const handleFullNameChange = (e) => {
+    setGeneralInfo({
+      ...generalInfo,
+      fullName: e.target.value
+    });
+  }
+
+  const handleEmailChange = (e) => {
+    setGeneralInfo({
+      ...generalInfo,
+      email: e.target.value
+    })
+  }
+
+  const handlePhoneNumberChange = (e) => {
+    setGeneralInfo({
+      ...generalInfo,
+      phoneNumber: e.target.value
+    })
+  }
+
+  return (
+    <main id='main'>
+      <Sidebar generalInfo={generalInfo} onFullNameChange={handleFullNameChange} onEmailChange={handleEmailChange} onPhoneNumberChange={handlePhoneNumberChange} />
+      <Preview generalInfo={generalInfo} />
+    </main>
+  )
+}
+
+function App() {
+  return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <Navbar />
+      <Main />
     </>
   )
 }
